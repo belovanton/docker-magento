93a1c0156cd60e5b814f1ac8c0038baea3eee12d
Anton Belov <belov_ab@kmplzt.de>
2016-06-23 13:01:15 +0400

    Update update_base_url.sh

--- docker-magento-c9-mysql/update_base_url.sh
+++ docker-magento-c9-mysql/update_base_url.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-sleep 30;
 echo "updating base url";
 mysql -uroot magento2 -e "update core_config_data set value=\"$BASE_URL\" where path like \"%base_url%\";"
 echo "clearing cache";
 rm -rf /var/www/current/var/cache/*
-echo "tail /var/log/lastlog";
-tail -f /var/log/lastlog
